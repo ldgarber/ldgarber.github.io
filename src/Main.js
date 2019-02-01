c37fb36c5821c8c662d380ac62d1b2282f118769
Leah Garber <leah.garber88@gmail.com>
2019-02-01 10:17:36 -0500

    adding routes and pages

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
+import {
+  Route, 
+  NavLink, 
+  HashRouter
+} from "react-router-dom"; 
+import Home from "./Home"; 
+import Stuff from "./Stuff"; 
+import Contact from "./Contact"; 
  
 class Main extends Component {
   render() {
     return (
+      <HashRouter>
         <div>
           <h1>Leah Garber</h1>
           <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><NavLink to="/">Home</NavLink>
+            <li><NavLink to="/stuff">Stuff</NavLink>
+            <li><NavLink to="/contact">Contact</NavLink>
           </ul>
           <div className="content">
-             
+            <Route path="/" component={Home}/> 
+            <Route path="/stuff" component={Stuff}/> 
+            <Route path="/contact" component={Contact}/> 
           </div>
         </div>
+      </HashRouter>
     );
   }
 }
