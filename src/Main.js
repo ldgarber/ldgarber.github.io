911fbeef333c1691a489f3a4555d9c80cae3d5dc
Leah Garber <leah.garber88@gmail.com>
2019-02-01 11:17:59 -0500

    some font styling

--- src/Main.js
+++ src/Main.js
@@ -13,7 +13,7 @@ class Main extends Component {
     return (
       <HashRouter>
         <div>
-          <h1>Leah Garber</h1>
+          <h1>Leah D. Garber</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/stuff">Stuff</NavLink></li>
