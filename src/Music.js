4da44b77226727cbe3658163641cbaf848ead392
Leah Garber <leah.garber88@gmail.com>
2019-02-06 13:04:13 -0500

    added Gallery with stock photos

--- src/Music.js
+++ src/Music.js
@@ -1,9 +1,12 @@
 import React from "react"; 
 import ContentPage from "./ContentPage"; 
+import Gallery from 'react-grid-gallery'; 
+import IMAGES from './_images'; 
 
 const Music = () => (
   <ContentPage>
     <p>Music</p>
+    <Gallery images={IMAGES} />
   </ContentPage>
 ); 
 
