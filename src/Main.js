fd1ba3250c508407ee045e8208db0026df30dbd6
Leah Garber <leah.garber88@gmail.com>
2019-02-12 15:49:23 -0500

    adding spotify link to Music page

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import {
   Route, 
+  Redirect, 
+  Switch, 
   HashRouter, 
 } from "react-router-dom"; 
 import Header from "./Header"; 
@@ -8,6 +10,8 @@ import Home from "./Home";
 import Code from "./Code";  
 import Music from "./Music"; 
 import About from "./About"; 
+
+const NotFoundRedirect = () => <Redirect to="/"/>
  
 class Main extends Component {
   render() {
@@ -16,10 +20,13 @@ class Main extends Component {
         <div>
           <Header />
           <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/code" component={Code}/> 
-            <Route path="/music" component={Music}/>
-            <Route path="/about" component={About}/>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/code" component={Code}/> 
+              <Route path="/music" component={Music}/>
+              <Route path="/about" component={About}/>
+              <Route component={NotFoundRedirect} />
+            </Switch>
           </div>
         </div>
       </HashRouter>
--- src/Music.js
+++ src/Music.js
@@ -3,8 +3,16 @@ import ContentPage from "./ContentPage";
 import Gallery from 'react-grid-gallery'; 
 import IMAGES from './constants/_images'; 
 
+const SpotifyPlayer = (props) => (
+  <div>
+    <iframe className="spotify-player centered" title="winter" src={props.src} width="300" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> 
+  </div>
+);  
+
 const Music = () => (
   <ContentPage>
+    <h2>Hear Me On</h2>
+    <SpotifyPlayer src="https://open.spotify.com/embed/album/0HK6rPJCdWLhj5lF7NFMiU" />
     <h2>Photo Gallery</h2>
     <Gallery  
       id="music-gallery"
