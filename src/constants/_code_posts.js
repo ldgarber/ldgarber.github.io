f43a5ebb4a1d5a057adb865231f306686c114755
Leah Garber <leah.garber88@gmail.com>
2019-04-15 16:01:37 -0400

    added code blog about top-city site

--- src/constants/_code_posts.js
+++ src/constants/_code_posts.js
@@ -8,6 +8,13 @@ const posts = [
   }, 
   {
     id: 1, 
+    title: "Restaurant Website", 
+    body: "Sample website for a bodega - made-up content, but allowed me to play around with React single page applications, React Router, and the Google Maps API.", 
+    link: "https://top-city.herokuapp.com", 
+    blog: ""
+  }, 
+  {
+    id: 2, 
     title: "React Memory Game", 
     body: "A card-flipping memory game I made in React.js.", 
     link: "https://leahdgarber.com/react-memory-game", 
